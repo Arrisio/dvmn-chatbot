f94b95ba651d8f3d2c3900b22f21df0b5de9d9b2
Dmitriy Suchkov <dsuchkov@jet.su>
2020-10-09 17:35:11 +0300

    feat: add checking response codes

--- main.py
+++ main.py
@@ -26,29 +26,37 @@ def run():
                 headers={"Authorization": f"Token {DVMN_TOKEN}"},
                 params={"timestamp": requested_timestamp},
             )
+            response.raise_for_status() # думаю, это исключение ловить на строке 55 except Exception as e:
             response_payload = response.json()
+
             if response_payload.get("status") == "found":
+                requested_timestamp = response_payload["last_attempt_timestamp"]
                 attempt_results = response_payload["new_attempts"]
                 logging.debug(
                     "devman api return attampts info",
                     extra={"attemptsNumber": len(attempt_results)},
                 )
-                requested_timestamp = response_payload["last_attempt_timestamp"]
-
                 notify_attempts_results(attempt_results)
+
             elif response_payload.get("status") == "timeout":
+                requested_timestamp = response_payload['timestamp_to_request']
                 logging.debug("devman api request rimeout")
+
             else:
                 raise Exception("unkown response format from devman api")
-        except (
-            requests.exceptions.ReadTimeout,
-            requests.exceptions.ConnectionError,
-        ) as e:
+
+        except requests.exceptions.ReadTimeout:
+            logging.debug("devman api request rimeout")
+
+        except requests.exceptions.ConnectionError as e:
             logging.warning(e.msg)
             time.sleep(CONNECTION_ERROR_SLEEP_TIME)
+
         except Exception as e:
             logging.exception("Fatal error in main loop", exc_info=True)
             raise e
+        # предполагаю, что отсылка к KISS была про этот блок
+        # если это избыточно, то как залогировать exception в соответсвии с настройками логгера?
 
 
 if __name__ == "__main__":
