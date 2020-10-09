0c49de17cf1e8b857d8857a466f1d7c77c102237
Dmitriy Suchkov <dsuchkov@jet.su>
2020-10-09 17:34:40 +0300

    style:

--- src/tlg.py
+++ src/tlg.py
@@ -3,6 +3,7 @@ import logging
 from typing import NoReturn
 import telegram
 
+
 def notify_attempts_results(
     attempts_results: list, bot=telegram.Bot(token=os.environ.get("TLG_BOT_TOKEN"))
 ) -> NoReturn:
@@ -13,7 +14,7 @@ def notify_attempts_results(
             parse_mode=telegram.parsemode.ParseMode.MARKDOWN,
             text=generate_message(attempt_result),
         )
-        logging.debug('attempt result notified', extra=attempt_result)
+        logging.debug("attempt result notified", extra=attempt_result)
 
 
 def generate_message(attempt_result: dict) -> str:
@@ -21,4 +22,7 @@ def generate_message(attempt_result: dict) -> str:
         check_result = "*возвращена на доработку*"
     else:
         check_result = "*принята*"
-    return f""" Задача [{attempt_result['lesson_title']}](https://dvmn.org{attempt_result["lesson_url"]}) {check_result}"""
+    return (
+        f"""Задача [{attempt_result['lesson_title']}]"""
+        f"""(https://dvmn.org{attempt_result["lesson_url"]}) {check_result}"""
+    )
