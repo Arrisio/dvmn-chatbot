522c54bff0c4a032968d343c5e36974723a2b5bd
Dmitriy Suchkov <dsuchkov@jet.su>
2020-10-14 12:03:20 +0300

    docs: corrext typo

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
+import logging.config
 import os
-import requests
-from datetime import datetime
 import time
+from datetime import datetime
+
+import requests
 
 from src.tlg import notify_attempts_results
-
-import logging.config
-
-logging.config.fileConfig("configs/logging.conf")
-
 
 DVMN_TOKEN = os.environ.get("DVMN_TOKEN")
 DVMN_API_URL = "https://dvmn.org/api/long_polling/"
@@ -26,27 +23,27 @@ def run():
                 headers={"Authorization": f"Token {DVMN_TOKEN}"},
                 params={"timestamp": requested_timestamp},
             )
-            response.raise_for_status() # думаю, это исключение ловить на строке 55 except Exception as e:
+            response.raise_for_status()  # думаю, это исключение ловить на строке 55 except Exception as e:
             response_payload = response.json()
 
             if response_payload.get("status") == "found":
                 requested_timestamp = response_payload["last_attempt_timestamp"]
                 attempt_results = response_payload["new_attempts"]
                 logging.debug(
-                    "devman api return attampts info",
+                    "devman api returns attempts info",
                     extra={"attemptsNumber": len(attempt_results)},
                 )
                 notify_attempts_results(attempt_results)
 
             elif response_payload.get("status") == "timeout":
-                requested_timestamp = response_payload['timestamp_to_request']
-                logging.debug("devman api request rimeout")
+                requested_timestamp = response_payload["timestamp_to_request"]
+                logging.debug("devman api request timeout")
 
             else:
                 raise Exception("unkown response format from devman api")
 
         except requests.exceptions.ReadTimeout:
-            logging.debug("devman api request rimeout")
+            logging.debug("devman api request timeout")
 
         except requests.exceptions.ConnectionError as e:
             logging.warning(e.msg)
@@ -60,4 +57,5 @@ def run():
 
 
 if __name__ == "__main__":
+    logging.config.fileConfig("configs/logging.conf")
     run()
